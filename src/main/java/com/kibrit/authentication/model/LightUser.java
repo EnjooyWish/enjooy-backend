23c1919f5802e7c0c9e5af11bfecb51fb604afd6
asuleymanova <arzu.suleymanova@kibrit.tech>
2021-06-24 17:04:05 +0400

    returned email field

--- src/main/java/com/kibrit/authentication/model/LightUser.java
+++ src/main/java/com/kibrit/authentication/model/LightUser.java
@@ -7,10 +7,12 @@ public class LightUser {
     private Long id;
     private String firstName;
     private String lastName;
+    private String email;
 
     public LightUser(User user) {
         this.id = user.getId();
         this.firstName = user.getFirstName();
         this.lastName = user.getLastName();
+        this.email = user.getEmail();
     }
 }
