337deb50756343f9142922f3d368b0a462e2e844
asuleymanova <arzu.suleymanova@kibrit.tech>
2022-02-25 13:55:03 +0400

    changed photo url

--- src/main/java/com/kibrit/authentication/mapper/UserMapper.java
+++ src/main/java/com/kibrit/authentication/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.kibrit.authentication.mapper;
 import com.kibrit.authentication.dto.UserDTO;
 import com.kibrit.authentication.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 
@@ -11,5 +12,8 @@ public interface UserMapper {
     UserMapper MAPPER = Mappers.getMapper(UserMapper.class);
 
     User fromDTO(UserDTO userDTO);
+
+
+    @Mapping(target="photo", source="user.photoUrl")
     UserDTO fromEntity(User user);
 }
